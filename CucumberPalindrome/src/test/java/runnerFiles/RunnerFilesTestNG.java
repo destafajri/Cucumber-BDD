f2136a16c3100989a9f631514dc0d3524587ffa9
Desta Fajri Dwi Purnomo <106084465+destafajri@users.noreply.github.com>
2022-07-18 19:59:00 +0700

    Delete CucumberPalindrome directory

--- CucumberPalindrome/src/test/java/runnerFiles/RunnerFilesTestNG.java
+++
@@ -1,9 +0,0 @@
-package runnerFiles;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features = "src/test/resources/featureFiles", glue = "stepdefs")
-public class RunnerFilesTestNG extends AbstractTestNGCucumberTests {
-
-}
